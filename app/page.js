57c20176189716a96ba0ac5966d63345a0f445b9
Fabian Malquinn <malquinnfabian@gmail.com>
2024-07-03 21:35:33 +0700

    :hammer: added footer feature

--- app/page.js
+++ app/page.js
@@ -2,11 +2,18 @@
 'use strict'
 import { Header } from "./lib/components/Header"
 import { Banner} from './lib/components/Banner'
+import { NavbarBottom } from "./lib/components/Navbar-Bottom"
+import { Footer } from "./lib/components/Footer"
+
+
+
 export default function App() {
   return (
     <>
       <Header/>
       <Banner/>
+      <Footer />
+      <NavbarBottom />
     </>
   )
 }
